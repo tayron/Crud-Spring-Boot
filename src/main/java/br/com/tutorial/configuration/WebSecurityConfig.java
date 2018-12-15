94c814a7dedff31e8bda4ea35b7b0411d8c7869b
Tayron <contato@tayron.com.br>
2018-12-15 20:33:27 -0200

    Desabilitando autenticação

--- src/main/java/br/com/tutorial/configuration/WebSecurityConfig.java
+++ src/main/java/br/com/tutorial/configuration/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 		http
 			.csrf().disable()
 			.authorizeRequests()
-				.antMatchers("/", "/index").permitAll()
+				.antMatchers("/**", "/index").permitAll()
 				.antMatchers("/institutions/**").hasAuthority("USER")
 				.antMatchers("/students/**").hasAuthority("USER")
 				.antMatchers("/users/**").hasAuthority("USER")
